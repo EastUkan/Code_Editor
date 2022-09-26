b589e3f12516212e3dd5de55799b3139c7e88f2d
Andrii <andkryvenko@gmail.com>
2022-09-26 19:11:35 +0300

    create app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import CodeBar from "./components/CodeBar/CodeBar";
+import { EditorProvider } from "./context/context";
+import styles from "./App.module.css";
+import Header from "./components/Header/Header";
+import Preview from "./components/Preview/Preview";
+
+function App() {
+  return (
+    <EditorProvider>
+      <CodeBar />
+
+      <div className={styles.main}>
+        <Header />
+        <Preview />
+      </div>
+    </EditorProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Webcam from "react-webcam";
+import Draggable from "react-draggable";
+import {
+  BsFillCameraVideoFill,
+  BsFillCameraVideoOffFill,
+} from "react-icons/bs";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  const [camera, setCamera] = useState(false);
+  return (
+    <>
+      <header className={styles.header}>
+        <button onClick={() => setCamera(!camera)} className={styles.button}>
+          {camera ? (
+            <BsFillCameraVideoFill fill="#fff" size={23} />
+          ) : (
+            <BsFillCameraVideoOffFill fill="#fff" size={23} />
+          )}
+        </button>
+      </header>
+      {camera ? (
+        <Draggable>
+          <Webcam
+            className={styles.camera}
+            audio={false}
+            width={300}
+            height={300}
+            videoConstraints={{
+              width: 300,
+              height: 300,
+              facingMode: "user",
+            }}
+          />
+        </Draggable>
+      ) : null}
+    </>
+  );
+}
+
+export default Header;
